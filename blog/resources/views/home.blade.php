d07e36d6dd0975f35436fd4e25daae5df2ba00b9
Ridwan Lawson <38885282+ridwanlawson@users.noreply.github.com>
2020-07-17 10:39:10 +0700

    Menu Sampai Dashboard Sudah Aman

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,24 +1,88 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Components &rsaquo; Hero &mdash; Stisla</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboardasdas</div>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <div>
-                        
+  <!-- CSS Libraries -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body class="layout-3">
+  <div id="app">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a style="color: white" class="nav-link" href="index.php"><b>Home</b></a>
+          </li>
+          <li class="nav-item">
+            <a style="color: white" class="nav-link" href="login.php">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav
+
+      <!-- Main Content -->
+      <div class="container main-content">
+        <section class="section">
+         
+            <div class="row">
+              <div class="col-12 mb-4">
+                <div class="hero align-items-center bg-primary text-white">
+                  <div class="hero-inner text-center">
+                    <h2>Menu Maniskum</h2>
+                    <p class="lead">Anda Berhasil Masuk ke Dalam Sistem Pilih Menu yang anda inginkan.</p>
+                    <div class="mt-4">
+                      <a href="/dashboard" class="form-control btn btn-outline-black btn-lg btn-icon icon-left"><i class="fas fa-sign-in-alt"></i> Masuk</a>
                     </div>
+                  </div>
                 </div>
+              </div> 
             </div>
-        </div>
+
+        </section>
+      </div>
+
     </div>
-</div>
-@endsection
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{ asset('assets/modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/modules/popper.js') }}"></script>
+  <script src="{{ asset('assets/modules/tooltip.js') }}"></script>
+  <script src="{{ asset('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('assets/modules/moment.min.js') }}"></script>
+  <script src="{{ asset('assets/js/stisla.js') }}"></script>
+  
+  <!-- JS Libraies -->
+
+  <!-- Page Specific JS File -->
+  
+  <!-- Template JS File -->
+  <script src="{{ asset('assets/js/scripts.js') }}"></script>
+  <script src="{{ asset('assets/js/custom.js') }}"></script>
+</body>
+</html>
