ed35a55ad3c68857992b9d4f32ceb7e24e137801
Ridwan Lawson <38885282+ridwanlawson@users.noreply.github.com>
2020-07-23 09:16:34 +0700

    sadas

--- blog/resources/views/layouts/skeleton.blade.php
+++ blog/resources/views/layouts/skeleton.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'Home') &mdash; {{ env('APP_NAME') }}</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
